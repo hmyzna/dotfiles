0bb01471770cfa1f679cc1bf50ac5a5a6830c92d
Anton Yakovlev <sei@disroot.org>
2024-11-03 15:42:38 +0300

    NVIM: Add lualine

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,11 @@
 require("config.lazy")
 
-vim.o.background = "dark"
+local o = vim.o
+
+o.expandtab = true
+o.smartindent = true
+o.tabstop = 2
+o.shiftwidth = 2
+
+o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
